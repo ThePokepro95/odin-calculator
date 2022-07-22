305d7e5bedc5ac954396ab2eb5a70948399e327f
Lawrence Isla <pokepro95@gmail.com>
2022-07-21 17:57:39 -0700

    Create operator functions

---
+++ code.js
@@ -0,0 +1,34 @@
+const add = (x, y) => x + y;
+
+const subtract = (x, y) => x - y;
+
+const multiply = (x, y) => x * y;
+
+const divide = (x, y) => x / y;
+
+const operate = (operator, x, y) => {
+    switch (operator){
+        case "+":
+            return add(x, y);
+            break;
+        case "-":
+            return subtract(x, y);
+            break;
+        case "*":
+            return multiply(x, y);
+            break;
+        case "/":
+            return divide(x, y);
+            break;
+    }
+
+}
+
+
+
+
+//--------Tests-------------//
+console.log(add(2, 59));
+console.log(add(0, 41));
+console.log(add(-5, 26));
+
